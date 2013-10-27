3d3b894f2cbfc452aa196e39d8f017f7d4258ccc
David Streever <dstreever@hortonworks.com>
2013-10-27 11:01:32 -0400

    Added hdp-repo recipe

---
+++ hdp-repo/attributes/default.rb
@@ -0,0 +1,23 @@
+default['hdp_repo']['base_repo_dir'] = '/var/www/html/repos'
+default['hdp_repo']['local_yum_repos_dir'] = "#{node['hdp_repo']['base_repo_dir']}/local.yum.repos.d"
+default['hdp_repo']['artifacts_dir'] = "#{node['hdp_repo']['base_repo_dir']}/artifacts"
+
+default['hdp_repo']['jdk_bin'] = "#{node['hdp_repo']['artifacts_dir']}/jdk-6u31-linux-x64.bin"
+default['hdp_repo']['jdk_source_bin'] = 'http://public-repo-1.hortonworks.com/ARTIFACTS/jdk-6u31-linux-x64.bin'
+
+default['hdp_repo']['ambari_repo_d'] = "/etc/yum.repos.d/ambari.repo"
+default['hdp_repo']['ambari_repo_artifact'] = "#{node['hdp_repo']['local_yum_repos_dir']}/ambari.repo"
+
+default['hdp_repo']['ambari_repo_source'] = "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/GA/ambari.repo"
+
+default['hdp_repo']['hdp_repo']['1.x']['d'] = "/etc/yum.repos.d/hdp.repo"
+default['hdp_repo']['hdp_repo']['1.x']['source'] = "http://public-repo-1.hortonworks.com/HDP/centos6/1.x/GA/hdp.repo"
+
+default['hdp_repo']['hdp_repo']['2.x']['d'] = "/etc/yum.repos.d/hdp2.repo"
+default['hdp_repo']['hdp_repo']['2.x']['source'] = "http://public-repo-1.hortonworks.com/HDP/centos6/2.x/GA/hdp.repo"
+
+default['hdp_repo']['hdp_repo_d'] = "/etc/yum.repos.d/hdp.repo"
+default['hdp_repo']['hdp_repo_artifact'] = "#{node['hdp_repo']['local_yum_repos_dir']}/hdp.repo"
+default['hdp_repo']['hdp_repo_source'] = "http://public-repo-1.hortonworks.com/HDP/centos6/1.x/GA/hdp.repo"
+
+default['hdp_repo']['repo_host'] = 'needs.to.be.overridden'
