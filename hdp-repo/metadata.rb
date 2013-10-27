4c915e0c18c45281434c8e8c90d888e2f60a80df
David Streever <dstreever@hortonworks.com>
2013-10-27 11:01:32 -0400

    Added hdp-repo recipe

---
+++ hdp-repo/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "David Streever"
+maintainer_email "david@streever.com"
+license          "Apache 2.0"
+description      "Create Local Repository to support HDP Installations"
+
+version          "0.1"
+
+depends "apache2"
